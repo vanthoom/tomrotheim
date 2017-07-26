0f9c8da92f2dde671455fdd36780fa6112d1fe58
Tom <mr.vanthoom@gmail.com>
2017-07-26 21:34:37 +0200

    format

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.set('view engine', 'pug');
 
 app.get('/', function(request, response) {
   response.render('pages/index');
@@ -16,5 +16,3 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
